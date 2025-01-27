8c6bcc2069862ac110357466349437d71097a993
Ciprian Serac <farazahar14@gmail.com>
2025-01-27 17:52:42 +0200

    Initial Layout

---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,27 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import Menu from '@/components/Menu';
+
+export default function DashboardLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className='h-screen flex'>
+      {/*LEFT*/}
+      <div className='w-1/6 md:w[8%] lg:w-[16%] xl:w-[14%] p-4'>
+        <Link
+          href='/'
+          className='flex items-center justify-center lg:justify-start gap-2'
+        >
+          <Image src='/logo.png' alt='logo' width={32} height={32} />
+          <span className='hidden lg:block'>Management Universitar</span>
+        </Link>
+        <Menu></Menu>
+      </div>
+      {/*RIGHT*/}
+      <div className='w-5/6 md:w[92%] lg:w-[86%] xl:w-[86%]'>R</div>
+    </div>
+  );
+}
